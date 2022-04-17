a3ce0295448a78296f71de4b75cda79c9f1bf0e0
MostafaMagdy99 <mostafaamagdii7@gmail.com>
2022-04-17 20:30:39 +0200

    External_Intterupt Test

---
+++ Led_Toogle/LED_Toggle/APP/LED_Toggle_Ext_INT/config_file.h
@@ -0,0 +1,39 @@
+/*
+ * config_file.h
+ *
+ *  Created on: Apr 17, 2022
+ *      Author: Seko
+ */
+
+#ifndef APP_LED_TOGGLE_EXT_INT_CONFIG_FILE_H_
+#define APP_LED_TOGGLE_EXT_INT_CONFIG_FILE_H_
+
+//#define LEDS_PORT	DIO_PORTA
+//#define LEDS_PORT	DIO_PORTB
+#define LEDS_PORT	DIO_PORTC
+//#define LEDS_PORT	DIO_PORTD
+
+#define LED_Pin0	0
+//#define LED_Pin1	1
+//#define LED_Pin2	2
+//#define LED_Pin3	3
+//#define LED_Pin4	4
+//#define LED_Pin5	5
+//#define LED_Pin6	6
+//#define LED_Pin7	7
+
+#define Switch_PORT		PORTA
+//#define Switch_PORT	DIO_PORTB
+//#define Switch_PORT	DIO_PORTC
+//#define Switch_PORT	DIO_PORTD
+
+#define Switch_Pin0		0
+//#define Switch_Pin1	1
+//#define Switch_Pin2	2
+//#define Switch_Pin3	3
+//#define Switch_Pin4	4
+//#define Switch_Pin5	5
+//#define Switch_Pin6	6
+//#define Switch_Pin7	7
+
+#endif /* APP_LED_TOGGLE_EXT_INT_CONFIG_FILE_H_ */
---
+++ Led_Toogle/LED_Toggle/Debug/APP/LED_Toggle_Ext_INT/main.d
@@ -0,0 +1,38 @@
+APP/LED_Toggle_Ext_INT/main.o APP/LED_Toggle_Ext_INT/main.o:  \
+ ../APP/LED_Toggle_Ext_INT/main.c \
+  ../APP/LED_Toggle_Ext_INT/../../MCAL/DIO/DIO_interface.h \
+  ../APP/LED_Toggle_Ext_INT/../../MCAL/DIO/../../LIB/STD_Types.h \
+  ../APP/LED_Toggle_Ext_INT/../../MCAL/DIO/../../LIB/BIT_MATH.h \
+  ../APP/LED_Toggle_Ext_INT/../../MCAL/DIO/../../SERVICE/DIO_register.h \
+  ../APP/LED_Toggle_Ext_INT/../../MCAL/External_interrupt/External_interrupt.h \
+  ../APP/LED_Toggle_Ext_INT/../../MCAL/External_interrupt/../../SERVICE/Interrupt_Vector.h \
+  ../APP/LED_Toggle_Ext_INT/../../MCAL/External_interrupt/../../SERVICE/DIO_register.h \
+  ../APP/LED_Toggle_Ext_INT/../../MCAL/External_interrupt/../../SERVICE/DIO_register.h \
+  ../APP/LED_Toggle_Ext_INT/../../MCAL/External_interrupt/../../LIB/BIT_MATH.h \
+  ../APP/LED_Toggle_Ext_INT/../../MCAL/External_interrupt/../../LIB/STD_Types.h \
+  ../APP/LED_Toggle_Ext_INT/../../LIB/BIT_MATH.h \
+  ../APP/LED_Toggle_Ext_INT/Config_file.h
+
+../APP/LED_Toggle_Ext_INT/../../MCAL/DIO/DIO_interface.h:
+
+../APP/LED_Toggle_Ext_INT/../../MCAL/DIO/../../LIB/STD_Types.h:
+
+../APP/LED_Toggle_Ext_INT/../../MCAL/DIO/../../LIB/BIT_MATH.h:
+
+../APP/LED_Toggle_Ext_INT/../../MCAL/DIO/../../SERVICE/DIO_register.h:
+
+../APP/LED_Toggle_Ext_INT/../../MCAL/External_interrupt/External_interrupt.h:
+
+../APP/LED_Toggle_Ext_INT/../../MCAL/External_interrupt/../../SERVICE/Interrupt_Vector.h:
+
+../APP/LED_Toggle_Ext_INT/../../MCAL/External_interrupt/../../SERVICE/DIO_register.h:
+
+../APP/LED_Toggle_Ext_INT/../../MCAL/External_interrupt/../../SERVICE/DIO_register.h:
+
+../APP/LED_Toggle_Ext_INT/../../MCAL/External_interrupt/../../LIB/BIT_MATH.h:
+
+../APP/LED_Toggle_Ext_INT/../../MCAL/External_interrupt/../../LIB/STD_Types.h:
+
+../APP/LED_Toggle_Ext_INT/../../LIB/BIT_MATH.h:
+
+../APP/LED_Toggle_Ext_INT/Config_file.h:
